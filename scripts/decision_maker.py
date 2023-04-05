8916ab900d190978792abb17551616594fcf8be6
Timotheevin <33911308+Timotheevin@users.noreply.github.com>
2023-04-05 14:27:06 +0200

    Update decision_maker.py

--- scripts/decision_maker.py
+++ scripts/decision_maker.py
@@ -1,286 +1,141 @@
 #!/usr/bin/env python
 
-## Ce programme gère la prise de décision globale et la communication avec les différents actionneurs.
+## Ce programme conserve map en mémoire.
+## Il la met à jour et maintient une veille d'évitement
 
-from pathfinding import *
 import rospy
 from robot_ROS.msg import Table_description
 from robot_ROS.msg import Object_position_description
+from robot_ROS.msg import Static_positioned_objects
 from std_msgs.msg import String
-import math
 import time
+import math
 
-#debuggage
-debug = True
+current_map = Table_description()
+current_map.itself.object = "false"
+current_map.annex.object = "false"
+current_map.opp_main.object = "false"
+current_map.opp_annex.object = "false"
 
-processing = False          #True si action interne en cours qui nécessite l'arrêt du robot (grab ou release de gateaux)
-moving = False              #True si le robot est actuellement en mouvement
-depose = False             #True à partir du moment où on a récupéré les 18 couches
-hastarget = True
-init_time = time.time()     #initialisation du temps à t = 0
-blocks_grabbable = 3        #nombre de block récupérable dans la pile de récupération
-blocks_grabbed = 0          #nombre de blocs récupérés (un bloc est un ensemble de 3 couches)
-cakes_released = 0          #nombre de gateaux relâchés (normalement bien formés)
-zones_plats = [(0,0)]*5     #coordonnées du centre des 5 plats par ordre d'importance (=les plus éloignés de la safezone) pour la fin de la partie (TODO)
-current_goal = [(0,0)]      #coordonnées de l'objectif actuel, à initialiser avec le premier point (1er bloc de ressources)
-equipe = 3                  #3:bleu et 0:vert
+current_map.other = Static_positioned_objects(q1_value = 2,
+                                                                      q2_value = 2,
+                                                                      q3_value = 2,
+                                                                      q4_value = 1,
+                                                                      q1 = [Object_position_description(object = "ingredient rose", x = 575, y = 2000-225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient yellow", x = 775, y = 2000-225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient brown", x = 1125, y = 2000-725, alpha = 0)],
+                                                                      q2 = [Object_position_description(object = "ingredient rose", x = 3000-575, y = 2000-225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient yellow", x = 3000-775, y = 2000-225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient brown", x = 3000-1125, y = 2000-725, alpha = 0)],
+                                                                      q3 = [Object_position_description(object = "ingredient rose", x = 3000-575, y = 225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient yellow", x = 3000-775, y = 225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient brown", x = 3000-1125, y = 725, alpha = 0)],
+                                                                      q4 = [Object_position_description(object = "ingredient rose", x = 575, y = 225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient yellow", x = 775, y = 225, alpha = 0),
+                                                                            Object_position_description(object = "ingredient brown", x = 1125, y = 725, alpha = 0)])
 
-#variables à ajuster au cours des tests
-time_to_pdp = 10            #temps nécessaire pour mettre les pieds dans le plat (pdp)
-calcul_frequency = 0.2      #Hertz
+stop = False
 
-def change_coordinates(new_origin, point):
-    # projection dans le repère du robot
-    projected = Object_position_description(object = point.object,
-                                            x = math.cos(new_origin.alpha)*point.x + math.sin(new_origin.alpha)*point.y, 
-                                            y = - math.sin(new_origin.alpha)*point.x + math.cos(new_origin.alpha)*point.y,
-                                            alpha = 0)
-    rot_origin = Object_position_description(object=new_origin.object,
-                                            x = math.cos(new_origin.alpha)*point.x + math.sin(new_origin.alpha)*point.y, 
-                                            y = - math.sin(new_origin.alpha)*point.x + math.cos(new_origin.alpha)*point.y,
-                                            alpha = 0)
-    projected.x -= rot_origin.x
-    projected.y -= rot_origin.y
-    return projected
+#TODO : init position of static objects
 
-def distance(origin, point):
-    #TODO : calcul de distance effective via algorithmithme de pathfinding pour amélioration
-    return math.sqrt((origin.x - point.x)**2 + (origin.y - point.y)**2)
-  
-def position_closest(list_objects, self_pos):
-    #careful: this function removes said objects from database 
-    dist = 3000
-    point = None
-    for i in range(len(list_objects)):
-        if distance(self_pos, list_objects[i]) <= dist:
-            point = i
-            dist = distance(self_pos, list_objects[i])
-    point = list_objects.pop(point)
-    return point
+
+
+def update_map(data):
+
+
+    global current_map
+    
+    # Mise à jour de la map avec comparaison à la map actuelle
+
+    trust_factor = 0.3 # facteur de confiance envers les données du Lidar
+    #TODO : rechercher le trust_factor optimal -- à voir si la bibliothèque le fait bcp mieux
+    #TODO : comparer la nouvelle position et l'ancienne pour éliminer les données aberrantes
+    #TODO : update status of static objects that become useless (by tracking position of opponent robots)
+
+    if current_map.itself.object == "false" :
+        current_map.itself = data.itself
+    else :
+        current_map.itself.x += trust_factor*(data.itself.x - current_map.itself.x)
+
+    if current_map.annex.object == "false" :
+        current_map.annex = data.annex
+    else :
+        current_map.annex.x += trust_factor*(data.annex.x - current_map.annex.x)
+
+    if current_map.opp_main.object == "false" :
+        current_map.opp_main = data.opp_main
+    else :
+        current_map.opp_main.x += trust_factor*(data.opp_main.x - current_map.opp_main.x)
+
+    if current_map.opp_annex.object == "false" :
+        current_map.opp_annex = data.opp_annex
+    else :
+        current_map.opp_annex.x += trust_factor*(data.opp_annex.x - current_map.opp_annex.x)
         
 
-def make_decision(data):
-    global depose, hastarget
-    #input : Table description, intern state, time left, moving, past actions
-    #output : first move (order to motors or order to actioners)
-    #publish for actioners to act
-    #l'ordre peut être de stopper tout mouvement jusqu'à validation par les actionneurs internes (état stop moteur)
-    
-    table_description = data
-    zones_availables = [[table_description.other.q1_value,     
-                         table_description.other.q1],
-                        [table_description.other.q2_value,
-                         table_description.other.q2],
-                        [table_description.other.q3_value,
-                         table_description.other.q3],
-                        [table_description.other.q4_value,
-                         table_description.other.q4]]        #zones de blocs encore disponibles (non défoncés par un robot adverse) (sens horaire, en partant des paniers)(2=True,1=target,0=False)
-    #moves possibles : aller chercher la ressource suivante, aller déposer les gateaux, mettre les pieds dans le plat
-    #si on est déjà en récupération de ressources, on privilégie de terminer la recup
-    time_left = 100 - (time.time() - init_time)
-    if time_left < time_to_pdp:
-        pieds_dans_le_plat(table_description)
-    else:
-        if not depose:
-            if blocks_grabbable == 0:
-                if blocks_grabbed == 6:
-                    depose = True
-                else:
-                    #réordonner les blocks
-                    pass
-            else:
-                for i in range(4):
-                    if zones_availables[i][0]==1:
-                        objective = position_closest(zones_availables[i][1], data.itself)
-                        move_steps = pathfinding.main(objective.x, objective.y, data, equipe)
-                        if debug:
-                            message = str(move_steps[1][0]) + str(move_steps[1][0])
-                            pub_debug.publish(message)
-                        start_move(move_steps[1][0], move_steps[1][1], data.itself)
-                        table_description.itself.x = move_steps[1][0]
-                        table_description.itself.y = move_steps[1][1]
-                        update_database(table_description)
-                        pass
-                        if len(zones_availables[i][1])==0:
-                            hastarget = False
-                            zones_availables[i][0]=0
-                            if i == 0:
-                                table_description.other.q1_value = 0
-              
-                            elif i == 1:
-                                table_description.other.q2_value = 0
-                        
-                            elif i == 2:
-                                table_description.other.q3_value = 0
-                           
-                            elif i == 3:
-                                table_description.other.q4_value = 0
-                
-                            update_database(table_description)
-
-                if not hastarget:
-                    if zones_availables[1][0] == 2 and zones_availables[2][0] == 2 :
-                        #go to closest
-                        #switch to ongoing
-                        pass
-                    #TODO: cas une seule zone available
-                    else:
-                        if zones_availables[equipe][0] == 2 :
-                            #go there
-                            zones_availables[equipe][0] = 1
-                            pass
-                        elif zones_availables[3-equipe][0] == 2 :
-                            #go there
-                            zones_availables[3-equipe][0] = 1
-                            pass
-                        else:
-                            #go to available from 1 and 2
-                            pass
-
-        else:
-            #aller poser dans les zones_plats en remplissant par le bas puis se placer en protection (passage en phase 3 de test plus tard)
-            pass
-              
-    message = "true"
-    pub_ask_data.publish(message)
         
-def pieds_dans_le_plat(table_description):
-    #effectue l'action de mettre les pieds dans le plat
-    for i in range(5):
-        if zone_libre(i):
-            type, x, y, rotation = get_move(zones_plats[i][0], zones_plats[i][1], table_description) #TODO
-            talker_motors(type, x, y, rotation)
-            break
+def update_objective(data):
 
+    global current_map
     
-def zone_libre(zone):
-    #regarde si la zone selectionnée est libre
-    #facile en regardant si les robots adverses intersectent la zone étudiée
-    pass
+    current_map = data
+    print(current_map)
 
 
-def start_move(x, y, self_pos, data):
-    #prend en entrée un point atteignable et renvoie les instructions de rotation + translation correspondants
-    objective = Object_position_description(object = "objective", x=x, y=y, alpha=0)
-    talker_motors(1, 0, 0, 180/math.pi*math.atan2(objective.y - self_pos.y, objective.x - self_pos.x) - self_pos.alpha)
-    talker_motors(2, distance(data.itself, objective), 0, 0)
+def send_data(data):
+    # Envoi de la map au decision_maker sur demande
+    if data.data == "true":
+        pub = rospy.Publisher('send_data', Table_description, queue_size=10)
+        pub.publish(current_map)
 
-def talker_motors(type, x, y, rotation):
-    # format des données à envoyer aux moteurs principaux : (int type, float x, float y, int rotation).
-    # type = (0:stop, 2:translation; 1:rotation)
-    # coordonnées dans la base du robot
-    # rotation sens trigo
-    
-    message = str(type) + "," + str(x) + "," + str(y) + "," + str(rotation)
-    pub_order_move.publish(message) #publish order to move
-    moving = True
-    
-    if debug:
-        message = "commande envoyee au moteur : " + message
-        pub_debug.publish(message)
-    
-def talker_actioners(grab):
-    # donne l'ordre aux actionneurs de se mettre en route pour ramasser les palets
-    # type = (true: grab, false: release)
-    message = ("true" if grab else "false")
-    pub_order_actioners.publish(message) #publish order to grab slices or to release them and put a cherry on it
-    processing = True
-    
-def feedback_move(data):
-    #reçoit le retour de l'arduino moteur (par ex si le mvmt a été effectué avec succès)
-    #lance un nouvel ordre instantanément
-    #reçoit "success" si c'est un succès, autre chose si c'est une erreur
-    
-    if debug:
-        message = "feedback moteurs : " + data.data
-        pub_debug.publish(message)
-				
-    if data.data == "success":
-        moving = False
-        message = "true"
-        pub_ask_data.publish(message)
-    else:
-        pass
-        #debug here
-    
-def intern_state(data):
-    #reçoit l'état interne du robot (actionneurs internes)
-    #lorsque les palets ont été ramassés avec succès, on peut lancer le prochain ordre de déplacement
-    #reçoit "success" si c'est un succès, autre chose si c'est une erreur
-    if data.data == "success":
-        processing = False
-        message = "true"
-        pub_ask_data.publish(message)
-    else:
-        pass
-        #debug here
-        
-def test_fct(data):
-    if debug:
-        message = "test starting"
-        pub_debug.publish(message)
-    table_description = data
-    zones_availables = [[table_description.other.q1_value,     
-                         table_description.other.q1],
-                        [table_description.other.q2_value,
-                         table_description.other.q2],
-                        [table_description.other.q3_value,
-                         table_description.other.q3],
-                        [table_description.other.q4_value,
-                         table_description.other.q4]]
-    
-    objective = position_closest(zones_availables[3][1], data.itself)
-    update_database(table_description)
-    
-    print(zones_availables[3][1])
-    
-    move_steps = pathfinder(objective.x, objective.y, data, equipe)
-    print(move_steps)
-    if debug:
-        message = "coordonnees a atteindre : x = " + str(move_steps[1][0]) + ", y = " + str(move_steps[1][1])
-        pub_debug.publish(message)
-        start_move(move_steps[1][0], move_steps[1][1], data.itself, data)
-       
-def starter_test(data):
-    message = "true"
-    pub_ask_data.publish(message)
+
+
+def check():
+    # Cette fonction renvoie true en cas de risque de collision
+    #TODO : collision with walls ?
+    #TODO : edit pour mettre des cercles au lieu des carrés ?
+
+    safety_distance = 200
+    #TODO : calculate optimal safety distance
+
+    if (current_map.annex.object != "false" and abs(current_map.itself.x - current_map.annex.x) < safety_distance
+                                            and abs(current_map.itself.y - current_map.annex.y) < safety_distance) :
+        return True
+    if (current_map.opp_main.object != "false" and abs(current_map.itself.x - current_map.opp_main.x) < safety_distance
+                                            and abs(current_map.itself.y - current_map.opp_main.y) < safety_distance) :
+        return True
+    if (current_map.opp_annex.object != "false" and abs(current_map.itself.x - current_map.opp_annex.x) < safety_distance
+                                            and abs(current_map.itself.y - current_map.opp_annex.y) < safety_distance) :
+        return True
+
+    return False
+
+
 
 def listener():
+    global stop
+    rospy.init_node('collision_monitor', anonymous=True)
+    
+    
+    rospy.Subscriber("update_objective", Table_description, update_objective)
+    rospy.Subscriber("update_data", Table_description, update_map)
+    rospy.Subscriber("ask_data", String, send_data)
 
     while not rospy.is_shutdown():
-    	if debug:
-            rate = rospy.Rate(100)
-            rate.sleep()
-    		
-    	else:
-            rate = rospy.Rate(calcul_frequency) 
-            message = "true"
-            pub_ask_data.publish(message)
-  
- 
-                
-def update_database(data):
-    pub_update.publish(data)
-             
+        # Veille évitement
+        if check():
+            if not stop:
+                stop = True
+                #TODO : envoyer directement à order_move l'ordre de s'arrêter sans passer par le decision_maker ?
+                #pub = rospy.Publisher('send_data', Table_description, queue_size=10)
+                pub = rospy.Publisher('order_move', String, queue_size=10)
+                #pub.publish() publish order to stop
+        elif stop:
+            stop = False;
+            pub = rospy.Publisher('order_move', String, queue_size=10)
+            #pub.publish() publish order to move again
+        time.sleep(0.05)
 
 
 if __name__ == '__main__':
-    global pub_ask_data, pub_debug
-
-    pub_ask_data = rospy.Publisher('ask_data', String, queue_size=10)
-    pub_debug = rospy.Publisher('debug', String, queue_size=10)
-    pub_update = rospy.Publisher('update_objective', Table_description, queue_size=10)
-    pub_order_move = rospy.Publisher('order_move', String, queue_size=10)
-    pub_order_actioners = rospy.Publisher('order_actioners', String, queue_size=10)
-
-    rospy.init_node('decision_maker', anonymous=True)
-
-    if debug:
-        rospy.Subscriber("send_data", Table_description, test_fct)
-    else:
-        rospy.Subscriber("send_data", Table_description, make_decision)
-    rospy.Subscriber("feedback_move", String, feedback_move)
-    rospy.Subscriber("intern_state", String, intern_state)
-    rospy.Subscriber("starter", String, starter_test)
-
     listener()
